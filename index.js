639a88a5ba7358d0521ffd586d0439c5c2ca00c7
dawood <dawood.altai111@gmail.com>
2023-04-16 04:05:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+console.log(findMatching(drivers,"Bobby"))
+console.log(findMatching(drivers,"Sammy"))
+console.log(findMatching(drivers,"dawood"))
+
+function fuzzyMatch(drivers, query) {
+    const queryLower = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(queryLower));
+  } 
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
+  
